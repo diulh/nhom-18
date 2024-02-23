48b7d825a6fefe59f3e66cb63fac12ae90424ebd
diulih <160729383+soclih@users.noreply.github.com>
2024-02-23 15:11:32 +0000

    assignment 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,12 +4,14 @@ class Number{
 }
 public class App {
     public static void main(String[] args) {
-        Number n1 = new Number();
-        Number n2 = new Number();
-        n1.i = 2;
-        n2.i = 5;
-        n1.i = n2.i;
-        n2.i = 10;
-        System.out.println("n1.i: " + n1.i);
+    Number n1 = new Number();
+    Number n2 = new Number();
+    n1.i = 2;
+    n2.i = 5;
+    n1 = n2;
+    n2.i = 10;// what is n1.i?
+    n1.i = 20;// what is n2.i?
+    System.out.println("n1.i: " + n1.i);
+    System.out.println("n2.i: " + n2.i);
     }
-}
+   }
