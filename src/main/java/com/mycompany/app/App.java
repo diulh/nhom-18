680889cd94017270af5edbef01a791b15c764724
diulih <160729383+soclih@users.noreply.github.com>
2024-02-23 15:04:03 +0000

    assignment 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,15 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
 package com.mycompany.app;
-
+class Number{
+    public int i;
+}
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1.i = n2.i;
+        n2.i = 10;
+        System.out.println("n1.i: " + n1.i);
     }
 }
